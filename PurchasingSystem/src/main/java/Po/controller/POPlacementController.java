54b632bbb59eed11c473653c62b53637f2d3650e
鮑俊丞 <jonathan66652@gmail.com>
2018-11-05 17:31:23 +0800

    下單功能完成

--- PurchasingSystem/src/main/java/Po/controller/POPlacementController.java
+++ PurchasingSystem/src/main/java/Po/controller/POPlacementController.java
@@ -1,7 +1,12 @@
 package Po.controller;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 
@@ -15,6 +20,7 @@ import Account.service.PO_Vendor_InfoService;
 import Apply.model.EmployeeBean;
 import Apply.service.App_MainService;
 import Apply.service.EmployeeService;
+import Po.model.PO_DetailBean;
 import Po.model.PO_MainBean;
 import Po.model.PO_SigningProcessBean;
 import Po.service.PO_DetailService;
@@ -46,7 +52,7 @@ public class POPlacementController {
 		String empid = bean1.getEmp_id();
 		String managerId = bean1.getEmp_managerid();
 		EmployeeBean mId = employeeService.select(managerId);
-		
+
 		List<PO_SigningProcessBean> signedOrderList1 = new LinkedList<PO_SigningProcessBean>();
 		List<PO_SigningProcessBean> signedOrderList2 = new LinkedList<PO_SigningProcessBean>();
 		List<PO_SigningProcessBean> signedOrderList3 = new LinkedList<PO_SigningProcessBean>();
@@ -87,10 +93,39 @@ public class POPlacementController {
 	public String signedOrderDetail(PO_SigningProcessBean bean, BindingResult bindingResult, Model model,
 			HttpSession session) {
 		String poid = bean.getPo_id();
+		
 		PO_MainBean pm = pO_MainService.select(poid);
-		model.addAttribute("poid", poid);
-		model.addAttribute("signedOrderDetail", pm);
+//		Set<PO_SigningProcessBean> po_Sign = pm.getpO_SigningProcessBean();
+		PO_SigningProcessBean po_Sign=pO_SigningProcessService.select("下單中", poid);
+		Set<PO_DetailBean> poDetail = pm.getpO_DetailBean();
+		model.addAttribute("pm", pm);
+		model.addAttribute("po_Sign", po_Sign);
+		model.addAttribute("poDetail", poDetail);
 		return "SignedOrderDetail.show";
 
+	}
+
+	@RequestMapping("/Po/signedOrderSubmit.controller")
+	public String signedOrderSubmit(PO_SigningProcessBean bean, BindingResult bindingResult, Model model,
+			HttpSession session, String send, String signSug, java.util.Date shippingDate) throws ParseException {
+		EmployeeBean bean1 = (EmployeeBean) session.getAttribute("user");
+		String empid = bean1.getEmp_id();
+		java.util.Date date = new java.util.Date();
+		java.sql.Date date1 = new java.sql.Date(date.getTime());
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String currentDate = dateFormat.format(date1);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date dateTime = sdf.parse(currentDate);
+		if (send.equals("送出")) {
+			PO_SigningProcessBean placeOrder = pO_SigningProcessService.select(bean.getPo_sta(), bean.getPo_id());
+			placeOrder.setSig_date(dateTime);
+			placeOrder.setSig_sta("已下單");
+			placeOrder.setSig_sug(signSug);
+			PO_SigningProcessBean placeOrder1 = pO_SigningProcessService.select("待收貨",bean.getPo_id());
+			placeOrder1.setSig_sta("出貨中");
+			PO_MainBean shippingUpdate = pO_MainService.select(bean.getPo_id());
+			shippingUpdate.setShipping_Date(shippingDate);
+		}
+		return "POlogin.success";
 	}
 }
--- PurchasingSystem/src/main/webapp/Po/SignedOrderDetail.jsp
+++ PurchasingSystem/src/main/webapp/Po/SignedOrderDetail.jsp
@@ -8,14 +8,43 @@
 <title>待下單明細</title>
 </head>
 <body>
-<h2>請購單編號:${signedOrderDetail.po_id}</h2>
-	<h2>下單產品明細</h2>
-	<c:forEach var='signedOrderDetail' items='${signedOrderDetail.pO_DetailBean}'>
-		<p>料號: ${signedOrderDetail.part_no}</p>
-		<p>料號: ${signedOrderDetail.productListBean.pro_name}</p>
-<%-- 		<p>產品平均單價: ${signedOrderDetail.pro_price}</p> --%>
-<%-- 		<p>請購數量: ${signedOrderDetail.app_amount}</p> --%>
-<%-- 		<p>該項總金額: ${signedOrderDetail.app_amount*partlist.pro_price}元整</p> --%>
-	</c:forEach>
+<%-- <h2>請購單編號:${signedOrderDetail.po_id}</h2> --%>
+<h2>請購單編號:${pm.po_id}</h2>
+	<h2>下單產品明細</h2>	
+	<form action="<c:url value="/Po/signedOrderSubmit.controller" />" method="post">
+<%-- 	<c:forEach var='signedOrderDetails' items='${signedOrderDetail}'> --%>
+
+<%-- 		<p>廠商名稱: ${signedOrderDetails.pO_Vendor_InfoBean.vendor_name}</p> --%>
+<%-- 		<p>料號: ${signedOrderDetails.pO_DetailBean.part_No}</p> --%>
+<%-- 		<p>產品名稱: ${signedOrderDetails.pO_DetailBean.productListBean.pro_name}</p> --%>
+<%-- 		<p>單價: ${signedOrderDetails.pO_DetailBean.quotation}</p> --%>
+<%-- 		<p>總價: ${signedOrderDetails.pO_DetailBean.total_Price}</p> --%>
+<%-- 		<p>總數量: ${signedOrderDetails.pO_DetailBean.total_Qty}</p> --%>
+<%-- 		</c:forEach> --%>
+
+		<c:forEach var='poDetail' items='${poDetail}'>
+		<p>廠商名稱: ${poDetail.pO_MainBean.pO_Vendor_InfoBean.vendor_name}</p>
+		<p>料號: ${poDetail.part_No}</p>
+		<p>產品名稱: ${poDetail.productListBean.pro_name}</p>
+		<p>單價: ${poDetail.quotation}</p>
+		<p>總價: ${poDetail.total_Price}</p>
+		<p>總數量: ${poDetail.total_Qty}</p>
+		</c:forEach>
+		
+<%-- 		<c:forEach var='po_Sign' items='${po_Sign}'> --%>
+		<input type='hidden' name='po_manger' value='${po_Sign.po_manger}'>
+		<input type='hidden' name='po_sta' value='${po_Sign.po_sta}'>
+		<input type='hidden' name='po_id' value='${po_Sign.po_id}'>
+		<input type='hidden' name='sig_date' value='${po_Sign.sig_date}'>
+		<input type='hidden' name='sig_sta' value='${po_Sign.sig_sta}'>
+		<input type='hidden' name='sig_sug' value='${po_Sign.sig_sug}'>
+		<input type='hidden' name='sig_rank' value='${po_Sign.sig_rank}'>
+<%-- 		</c:forEach> --%>
+下單意見:<p><textarea rows="5" cols="50" name="signSug"></textarea></p>
+預計出貨時間:<input type="datetime" name="shippingDate">
+<p><input type="submit" name="send" value="送出">
+		</form>
+
+
 </body>
 </html>
