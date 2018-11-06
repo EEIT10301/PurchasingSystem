5b266931b5c1a2ed17c052cab43909499fd1af52
劉亮廷 <tim810320@gmail.com>
2018-11-06 18:41:45 +0800

    修改 product controller

--- PurchasingSystem/src/main/java/Apply/controller/ApplyProductController.java
+++ PurchasingSystem/src/main/java/Apply/controller/ApplyProductController.java
@@ -1,6 +1,10 @@
 package Apply.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -36,18 +40,27 @@ public class ApplyProductController {
 		return new JSONArray(json);
 	}
 	@RequestMapping(value="/SearchCus.do", produces ="text/html; charset=utf-8" )
-	public 	@ResponseBody String SelectAllAJAX() throws IOException {
+	public 	@ResponseBody String SelectAllAJAX() throws IOException, ParseException {
 		//response.setContentType("text/html;charset=UTF-8");
 		 List<ProductListBean>  list = null;
 		list = productListService.selectAll();
 		List<ProductListBean>  lists = new LinkedList<ProductListBean>();
+//		for(int i=0;i<list.size();i++) {
+//			ProductListBean xs= list.get(i);
+//			DateFormat dateFormate =new SimpleDateFormat("yyyy/MM/dd");
+//			String now= dateFormate.format(xs.getPro_date());
+//			SimpleDateFormat sdf =new SimpleDateFormat("yyyy/MM/dd");
+//			Date datas=sdf.parse(now);
+//			xs.setPro_date(datas);
+//			lists.add(xs);
+//		}	
 		lists=list;
 		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create() ; 
 		String json = gson.toJson(lists) ;
 	    return json;
 	}
 	@RequestMapping(value="/RemoveCus.do", produces ="text/html; charset=utf-8" )
-	public 	@ResponseBody String UpdateAJAX(HttpServletRequest request) throws IOException {
+	public 	@ResponseBody String UpdateAJAX(HttpServletRequest request) throws IOException, ParseException {
 		String jsonStr = readJSON(request);
 		JSONObject jsonObj = new JSONObject(jsonStr);
 		 ProductListBean em = null;
@@ -58,11 +71,18 @@ public class ApplyProductController {
 		Integer pro_price = Integer.valueOf(jsonObj.getString("tel"));
 		Integer pro_amount = Integer.valueOf(jsonObj.getString("adr"));
 		em=productListService.select(partno);
+		java.util.Date date = new java.util.Date();
+		java.sql.Date data1 = new java.sql.Date(date.getTime());
+		DateFormat dateFormate =new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String now= dateFormate.format(data1);
+		SimpleDateFormat sdf =new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date datas=sdf.parse(now);
 //		em.setPro_cate(pro_cate);
 //		em.setPro_name(pro_name);
 //		em.setPro_spe(pro_spe);
 		em.setPro_price(pro_price);
 		em.setPro_amount(pro_amount);
+		em.setPro_date(datas);
 		return "1";
 	}
 
