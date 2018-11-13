33e56a8fb0c34db2294133d99ff02eb4a00deff2
呂健銘 <100412083@stud.sju.edu.tw>
2018-11-13 11:18:22 +0800

    詢價修改

--- PurchasingSystem/src/main/java/Po/controller/POPlacementController.java
+++ PurchasingSystem/src/main/java/Po/controller/POPlacementController.java
@@ -91,15 +91,13 @@ public class POPlacementController {
 
 	@RequestMapping("/Po/signedOrderDetail.controller")
 
-
-
-	public String signedOrderDetail(PO_SigningProcessBean bean, BindingResult bindingResult, Model model,
+	public String signedOrderDetail(PO_SigningProcessBean bean, BindingResult bindingResult, Model model,String po_id,
 			HttpSession session) {
 
-	String poid = bean.getPo_id();
-		PO_MainBean pm = pO_MainService.select(poid);
+
+		PO_MainBean pm = pO_MainService.select(po_id);
 //		Set<PO_SigningProcessBean> po_Sign = pm.getpO_SigningProcessBean();
-		PO_SigningProcessBean po_Sign = pO_SigningProcessService.select("下單中", poid);
+		PO_SigningProcessBean po_Sign = pO_SigningProcessService.select("下單中", po_id);
 		Set<PO_DetailBean> poDetail = pm.getpO_DetailBean();
 		model.addAttribute("pm", pm);
 		model.addAttribute("po_Sign", po_Sign);
